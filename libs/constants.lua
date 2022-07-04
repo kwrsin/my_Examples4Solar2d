ead10257d2c4af0493c4daceec76e3a77004720c
kwrsin <kwrsin@gmail.com>
2022-07-04 12:53:58 +0900

    support a polygon collider in tile layer.

--- libs/constants.lua
+++ libs/constants.lua
@@ -1,5 +1,6 @@
 -- constants.lua
 return {
+	-- contents
 	cx = display.contentCenterX,
 	cy = display.contentCenterY,
 	width = display.contentWidth,
@@ -7,15 +8,20 @@ return {
 	actualWidth = display.actualContentWidth,
 	actualHeight = display.actualContentHeight,
 
+	-- tile data
 	tilesetPath = "assets/levels/",
 	imagePath = "assets/images/",
 	imageDotPath = "assets.images.",
+	layerType_tile = 'tilelayer',
+	layerType_object = 'objectlayer',
 
+	-- actor's role
 	role_player = "player",
 	role_enemy = "enemy",
 	role_npc = "npc",
 	role_item = "item",
 
+	-- scenarios
 	scenario_hello = 1,
 
 }
--- libs/levelLoader.lua
+++ libs/levelLoader.lua
@@ -20,6 +20,51 @@ local function imageSheet(tileset)
 		} )
 end
 
+local function multiPolygon(tileset)
+	local meshList = {}
+	local tiles = tileset.tiles
+	if tiles and #tiles > 0 then
+		for i, tile in ipairs(tiles) do
+			local mesh = {}
+			mesh.gid = tileset.firstgid + tile.id
+			local objectGroup = tile.objectGroup
+			if objectGroup and objectGroup.type == 'objectgroup' then
+				local objects = objectGroup.objects
+				local colliderList = {}
+				for j, object in ipairs(objects) do
+					if object.shape == 'polygon' and #object.polygon > 0 then
+						local vertexList = {}
+						for k, vertex in ipairs(object.polygon) do
+							vertexList[#vertexList + 1] = vertex.x - object.x
+							vertexList[#vertexList + 1] = vertex.y - object.y
+						end
+						colliderList[#colliderList + 1] = vertexList
+					elseif object.shape == 'rectangle' then
+						-- TODO: other shape
+					end
+				end
+				mesh.colliderList = colliderList
+
+			else
+				-- TODO: a single object
+			end
+			meshList[#meshList + 1] = mesh
+		end
+	end
+	return meshList
+end
+
+local function loadColliders(tilesets)
+	local mpList = {}
+	for i, tileset in ipairs(tilesets) do
+		local mpvalues = multiPolygon(tileset)
+		for j, mp in ipairs(mpvalues) do
+			mpList[mp.gid] = mp.colliderList
+		end
+	end	
+	return mpList
+end
+
 local function loadTiles(tilesets)
 	local tilesetList = {}
 	for i, tileset in ipairs(tilesets) do
@@ -36,9 +81,9 @@ end
 	Add Actors
 	Don't forget a "return" after actor createing.
 --]]
-local function createTile(group, tilesets, gid, x, y, layerIndex)
+local function createTile(group, tilesets, gid, x, y, colliders)
 	function localIndex()
-		return gid - 256 * (layerIndex - 1)
+		return gid % 256
 	end
 	local lid = localIndex()
 	function createATile()
@@ -48,8 +93,17 @@ local function createTile(group, tilesets, gid, x, y, layerIndex)
 		local aWall = display.newImage(group, tilesets[gid], lid, x, y)
 		physics.addBody(aWall, 'static', {})
 	end
+	function createAWallWithPolygon()
+		local aWall = display.newImage(group, tilesets[gid], lid, x, y)
+		local gidCollider = colliders[gid]
+		for _, collider in ipairs(gidCollider) do
+			physics.addBody(aWall, 'static', {shape=collider})
+		end
+	end
 
-	if gid == 256 + 0 + 1 then
+	if gid == 256 + 3 + 1 then
+		createAWallWithPolygon()
+	elseif gid == 256 + 0 + 1 then
 		redHeadGenerate({group=group, x=x, y=y, disabled=true, scenario_index=const.scenario_hello})
 	elseif gid == 256 + 2 + 1 then
 		reibaishiGenerate({group=group, x=x, y=y, disabled=true, isPlayer=true})
@@ -70,25 +124,30 @@ local function load(sceneGroup)
 	local level_path = appStatus.level_path
 	local level = require(level_path)
 	local tilesets = loadTiles(level.tilesets)
+	local colliders = loadColliders(level.tilesets)
 	local tilewidth = level.tilewidth
 	local tileheight = level.tileheight
 	for i, layer in ipairs(level.layers) do
 		local index = 1
 		local layerGroup = display.newGroup()
-		for row = 1, layer.height do
-			for col = 1, layer.width do
-				local gid = layer.data[index]
-				if gid > 0 then
-					createTile( 
-						layerGroup, 
-						tilesets, 
-						gid,
-						(col - 1) * tilewidth + tilewidth / 2,
-						(row - 1) * tileheight + tileheight / 2,
-						i)
+		if layer.type == const.layerType_tile then
+			for row = 1, layer.height do
+				for col = 1, layer.width do
+					local gid = layer.data[index]
+					if gid > 0 then
+						createTile( 
+							layerGroup, 
+							tilesets, 
+							gid,
+							(col - 1) * tilewidth + tilewidth / 2,
+							(row - 1) * tileheight + tileheight / 2,
+							colliders)
+					end
+					index = index + 1
 				end
-				index = index + 1
 			end
+		elseif layer.type == const.layerType_object then
+
 		end
 		camera:add(layerGroup, -i + 3) 
 	end
