2fc04df091020e5f90ef9a917b727e5e1e6b79fe
kwrsin <kwrsin@gmail.com>
2022-07-06 15:37:40 +0900

    Create testScene.lua

---
+++ scenes/tests/testScene.lua
@@ -0,0 +1,38 @@
+-- testScene.lua
+local const = require('libs.constants')
+local composer = require('composer')
+local redHeadGenerate = require('game_objects.actors.redHead')
+
+local scene = composer.newScene()
+local physics = require('physics')
+local appStatus = require('libs.appStatus')
+
+local player
+
+function scene:create(event)
+	local sceneGroup = self.view
+
+	physics.start(false)
+	player = redHeadGenerate({group=sceneGroup, x=const.cx, y=const.cy, disabled=false})
+end
+
+function scene:show(event)
+	if event.phase == 'will' then
+		appStatus.controller.show()
+		appStatus.manager.setPlayer(player)
+		appStatus.manager.setWorldBoundary(const.actualWidth, const.actualHeight)
+	elseif event.phase == 'did' then
+	end
+end
+
+function scene:hide(event)
+	if event.phase == 'will' then
+	elseif event.phase == 'did' then
+	end
+end
+
+scene:addEventListener( 'create', scene )
+scene:addEventListener( 'show', scene )
+scene:addEventListener( 'hide', scene )
+
+return scene
